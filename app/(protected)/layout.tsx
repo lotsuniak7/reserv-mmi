df6111fff325cbdd1bcfb3f8f44b6b2f64d3e264
lotsuniak7 <ivanuska961@gmail.com>
2025-11-27 14:43:55 +0100

    fixing bags

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -3,7 +3,6 @@ import Header from "@/components/Header";
 import { cookies } from "next/headers";
 import { createServerClient } from "@supabase/ssr";
 import { redirect } from "next/navigation";
-import "../globals.css";
 
 export default async function ProtectedLayout({ children }: { children: React.ReactNode }) {
     const cookieStore = await cookies();
@@ -13,8 +12,13 @@ export default async function ProtectedLayout({ children }: { children: React.Re
         process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
         {
             cookies: {
-                get(name: string) {
-                    return cookieStore.get(name)?.value;
+                getAll() { return cookieStore.getAll(); },
+                setAll(cookiesToSet) {
+                    try {
+                        cookiesToSet.forEach(({ name, value, options }) =>
+                            cookieStore.set(name, value, options)
+                        );
+                    } catch {}
                 },
             },
         }
@@ -24,13 +28,21 @@ export default async function ProtectedLayout({ children }: { children: React.Re
     if (!user) redirect("/auth/login");
 
     return (
-        <div className="min-h-screen bg-[var(--background)] bg-red-200">
+        // flex-row не нужен, так как Sidebar fixed.
+        <div className="min-h-screen bg-[var(--background)]">
+
+            {/* Сайдбар фиксирован, он не занимает места в потоке */}
             <Sidebar />
-            <div className="bg-teal-50 w-72 h-screen"></div>
-            {/* Используем кастомный CSS класс main-content-wrapper из globals.css */}
-            <div className=" bg-amber-400 ml-32 w-full h-screen">
-                <main className="p-6">
+
+            {/* ГЛАВНОЕ:
+               1. md:ml-[260px] -> сдвигает весь контент вправо ровно на ширину сайдбара.
+               2. min-h-screen -> растягивает контент на всю высоту.
+            */}
+            <div className="md:ml-[260px] min-h-screen">
+                <main className="flex-1 p-6">
                     <Header />
+
+                    {/* Контент страницы (твои материалы) */}
                     <div className="mt-6">
                         {children}
                     </div>
